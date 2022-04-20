ede8ca4469f10c094517f3af5a33c0041744c8de
Sujin Kim <31465594+sujin7837@users.noreply.github.com>
2022-04-20 18:03:34 +0900

    Update and rename BOJ_17779_게리맨더링.java to BOJ_17779_게리맨더링2.java

--- BOJ/src/구현/BOJ_17779_게리맨더링2.java
+++ BOJ/src/구현/BOJ_17779_게리맨더링2.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_17779_게리맨더링 {
+public class BOJ_17779_게리맨더링2 {
 
 	private static BufferedReader br;
 	private static StringTokenizer st;
