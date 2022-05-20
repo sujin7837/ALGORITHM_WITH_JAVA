35b6e0e313ea9f5a60dbac67e2dde9fe797073cb
Sujin Kim <31465594+sujin7837@users.noreply.github.com>
2022-05-21 01:08:07 +0900

    Create BOJ_16195_123더하기9.java

---
+++ BOJ/src/다이나믹프로그래밍/BOJ_16195_123더하기9.java
@@ -0,0 +1,42 @@
+package 다이나믹프로그래밍;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_16195_123더하기9 {
+
+	private static BufferedReader br;
+	private static StringTokenizer st;
+	
+	private static int T, N, M;
+	private static long [][]dp=new long[1001][1001];
+	
+	public static void main(String[] args) throws IOException {
+		br=new BufferedReader(new InputStreamReader(System.in));
+		T=Integer.parseInt(br.readLine());
+		for(int t=1;t<=T;t++) {
+			st=new StringTokenizer(br.readLine());
+			N=Integer.parseInt(st.nextToken());
+			M=Integer.parseInt(st.nextToken());
+			
+			dp[1][1]=1;
+			dp[2][1]=1;
+			dp[2][2]=1;
+			dp[3][1]=1;
+			dp[3][2]=2;
+			dp[3][3]=1;
+			for(int i=4;i<=N;i++) {
+				for(int j=1;j<=i;j++) {
+					if(j>M) break;
+					dp[i][j]=(dp[i-1][j-1]+dp[i-2][j-1]+dp[i-3][j-1])%1000000009;
+				}
+			}
+			long result=0;
+			for(int i=1;i<=M;i++) result=(result+dp[N][i])%1000000009;
+			System.out.println(result);
+		}
+	}
+
+}
